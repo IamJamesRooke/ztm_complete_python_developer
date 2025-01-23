21dd12d5d82d4378769fcd33f690e59f75bfbb41
James Rooke <james.harling@outlook.com>
2025-01-23 12:02:00 -0500

    finished unittest lessons

--- 12_testing/test2.py
+++ 12_testing/test2.py
@@ -1,22 +1,9 @@
-#python -m unittest -v calls comments as well
-
 import unittest
 import main
 
 # standard way of working with unittest
 class TestMain(unittest.TestCase):
-    
-    def setUp(self) -> None:
-        test_method = getattr(self, self._testMethodName)
-        print(f'\nRUNNING: {self._testMethodName.upper()}\n**********')
-        print(f'Docstring: {test_method.__doc__ or "No description"}')
-        return super().setUp()
-    
-    
     def test_do_stuff(self):
-        """
-        Docstrings will be included like this.
-        """
         test_param = 10
         result = main.do_stuff(test_param)
         self.assertEqual(result, 15)
@@ -30,10 +17,6 @@ class TestMain(unittest.TestCase):
         test_param = None
         result = main.do_stuff(test_param)
         self.assertEqual(result, 'Please enter number.')
-
-    def tearDown(self) -> None:
-        print('***************************************************')
-        print('DONE')
 
 if __name__ == '__main__':
     unittest.main()
