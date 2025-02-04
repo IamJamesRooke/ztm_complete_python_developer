1289900eb9c4549b886fb23e8461c8609f2f2fd3
James Rooke <me@jamesrooke.blog>
2025-02-04 10:38:05 -0500

    learning jinja and templating for web server

--- 15_web_development/web server/server.py
+++ 15_web_development/web server/server.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
-print(__name__)
+app.config['DEBUG'] = True  # Enable debug mode
 
-""" @app.route('/')
-def hello_world():
-    return 'Hello, World!' """
+@app.route('/')
+def index():
+    print(url_for('static', filename='favicon.ico'))
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)  # Enable debug mode when running directly
