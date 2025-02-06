af17b142ff0d16e9e20b738a8390f7830b914d3b
James Rooke <me@jamesrooke.blog>
2025-02-06 11:54:10 -0500

    created write_to_file fucntion for server.py

--- 15_web_development/universe/server.py
+++ 15_web_development/universe/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect
+import os
 app = Flask(__name__, static_folder='static')
 app.config['DEBUG'] = True  # Enable debug mode
 
@@ -11,10 +12,20 @@ def my_home():
 def page(page_name):
     return render_template(f'{page_name}')
 
+def write_to_file(data):
+    current_folder = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(current_folder, 'database.csv')
+    with open(file_path, mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        database.write(f'{email}, {subject}, {message}\n')
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
+        write_to_file(data)
         return redirect('/thankyou.html')
     else:
         return 'Error: Form submission failed!'
